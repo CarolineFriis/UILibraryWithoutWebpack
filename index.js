7a29ae8929531d7e15f43c8faac57143a4f822a9
CarolineFriis <89904084+CarolineFriis@users.noreply.github.com>
2021-12-12 19:30:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import Tooltip from './ninja-ui/tooltip';
-import Dropdown from './ninja-ui/dropdown';
-import Tabs from './ninja-ui/tabs';
-import Snackbar from './ninja-ui/snackbar';
-
 //create a tooltip
 const tooltip = new Tooltip (document.querySelector('.tooltip'));
 
